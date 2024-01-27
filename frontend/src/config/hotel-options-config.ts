862636673235d7b77e95fcecd37a43d867f552ba
Mahmoud Mohamed <95969990+MAHMOUD1122q@users.noreply.github.com>
2024-01-27 18:27:06 +0200

    Add files via upload

---
+++ frontend/src/config/hotel-options-config.ts
@@ -0,0 +1,22 @@
+export const hotelTypes = [
+  "Budget",
+  "Boutique",
+  "Luxury",
+  "Ski Resort",
+  "Business",
+  "Family",
+  "Romantic",
+  "Hiking Resort",
+  "Cabin",
+  "Beach Resort",
+  "Golf Resort",
+  "Motel",
+];
+
+export const hotelFacilities = [
+  "Free WiFi",
+  "Parking",
+  "Airport Shuttle",
+  "Family Rooms",
+  "Non-Smoking Rooms",
+];
